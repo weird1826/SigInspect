954dd1e06bda35f623570aa2fba29f21a2a21b2a
Tejas Parmar <parmart1@roehampton.ac.uk>
2026-02-12 21:36:31 +0000

    Function to open a file

--- siginspect.c
+++ siginspect.c
@@ -1,7 +1,24 @@
 #include<stdio.h>
 #include<string.h>
 
+void open_file(const char *filepath){
+    FILE *f = fopen(filepath, "rb");
+
+    if(f == NULL) {
+        fprintf(stderr, "Error opening file %s: ", filepath);
+        perror("");
+        return;
+    }
+
+    printf("File opened successfully!");
+}
+
 int main(int argc, char *argv[]){
-    printf("Hello World!");
+    if(argc != 2) {
+        fprintf(stderr, "Usage: %s <file_to_scan>\n", argv[0]);
+    }
+
+    open_file(argv[1]);
+
     return 0;
 }
